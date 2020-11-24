5332f084d17ef46079d734e7ef3110f3711ee6aa
Cader <mcader@hasnatech.com>
2020-11-24 16:52:06 +0530

    added quiz

---
+++ src/app/pages/game/data.js
@@ -0,0 +1,89 @@
+questions = [{
+    type: 'single_choice',
+    title: 'Customer Care Management',
+    question: 'You receive an order request from the Customer asking Cargill to deliver goods. What steps should you follow before the order is entered in our systems?',
+    instr: 'Choose the correct option.',
+    selected: -1,
+    submitted: false,
+    choices: [{
+        text: 'Promptly ensure everything is set-up correctly in our systems so the Customer has a seamless experience.',
+        correct: true,
+
+      },
+      {
+        text: 'Read the request notes carefully. Perform updates exactly as they are outlined. Do nothing further.',
+        correct: false,
+      },
+      {
+        text: 'Hold the order request. Contact the Customer to double check and review all the details they submitted. Document your notes in a notebook or use post-it notes, and then save them to enter when you have all your responses from the Customer.',
+        correct: false,
+      },
+      {
+        text: 'Review and double check the details with the Customer. Wait to enter the details into the system until the day before the loading date.',
+        correct: false,
+      }
+    ],
+    feedback: {
+      correct: `<h2 class='correct'>Great job! You are correct.</h2>
+        <p>When we promptly ensure everything is set-up correctly in our systems, we enable our customers to consistently experience a seamless Order Management process. Here are a few actions to keep in mind:</p>
+        <ul>
+          <li>After completing requests, proactively check other key areas, like the Master data fields, to ensure updates are made to applicable key systems so seamless order process occurs that allows for automation of tasks. When this occurs successfully, CSRs can spend more time engaging with the customers.</li>
+          <li>Validate the accuracy of key details like Ship To, Special Requests, confirming the products requested go to the right place and that their Incoterms are set up correctly. </li>
+          <li>Occasionally, for some manual requests you may review an invoice to validate details. </li>
+          <li>When a new customer is being setup, collaborate with the Commercial team and ensure that Service Level Agreements (SLA) are embedded in all systems in alignment with customer segmentation (where applicable).</li>
+        </ul>`,
+      incorrect: `<h2 class='correct'>Good try, but this is incorrect. Option A is the correct answer.</h2>
+        <p>When we promptly ensure everything is set-up correctly in our systems, we enable our customers to consistently experience a seamless Order Management process. Here are a few actions to keep in mind:</p>
+        <ul>
+          <li>After completing requests, proactively check other key areas, like the Master data fields, to ensure updates are made to applicable key systems so seamless order process occurs that allows for automation of tasks. When this occurs successfully, CSRs can spend more time engaging with the customers.</li>
+          <li>Validate the accuracy of key details like Ship To, Special Requests, confirming the products requested go to the right place and that their Incoterms are set up correctly. </li>
+          <li>Occasionally, for some manual requests you may review an invoice to validate details. </li>
+          <li>When a new customer is being setup, collaborate with the Commercial team and ensure that Service Level Agreements (SLA) are embedded in all systems in alignment with customer segmentation (where applicable).</li>
+        </ul>`
+    }
+  },
+  {
+    type: 'single_choice',
+    title: 'Customer Care Management',
+    question: '2. You receive an order request from the Customer asking Cargill to deliver goods. What steps should you follow before the order is entered in our systems?',
+    instr: 'Choose the correct option.',
+    selected: -1,
+    submitted: false,
+    choices: [{
+        text: 'Promptly ensure everything is set-up correctly in our systems so the Customer has a seamless experience.',
+        correct: true,
+
+      },
+      {
+        text: 'Read the request notes carefully. Perform updates exactly as they are outlined. Do nothing further.',
+        correct: false,
+      },
+      {
+        text: 'Hold the order request. Contact the Customer to double check and review all the details they submitted. Document your notes in a notebook or use post-it notes, and then save them to enter when you have all your responses from the Customer.',
+        correct: false,
+      },
+      {
+        text: 'Review and double check the details with the Customer. Wait to enter the details into the system until the day before the loading date.',
+        correct: false,
+      }
+    ],
+    feedback: {
+      correct: `<h2 class='correct'>Great job! You are correct.</h2>
+        <p>When we promptly ensure everything is set-up correctly in our systems, we enable our customers to consistently experience a seamless Order Management process. Here are a few actions to keep in mind:</p>
+        <ul>
+          <li>After completing requests, proactively check other key areas, like the Master data fields, to ensure updates are made to applicable key systems so seamless order process occurs that allows for automation of tasks. When this occurs successfully, CSRs can spend more time engaging with the customers.</li>
+          <li>Validate the accuracy of key details like Ship To, Special Requests, confirming the products requested go to the right place and that their Incoterms are set up correctly. </li>
+          <li>Occasionally, for some manual requests you may review an invoice to validate details. </li>
+          <li>When a new customer is being setup, collaborate with the Commercial team and ensure that Service Level Agreements (SLA) are embedded in all systems in alignment with customer segmentation (where applicable).</li>
+        </ul>`,
+      incorrect: `<h2 class='correct'>Good try, but this is incorrect. Option A is the correct answer.</h2>
+        <p>When we promptly ensure everything is set-up correctly in our systems, we enable our customers to consistently experience a seamless Order Management process. Here are a few actions to keep in mind:</p>
+        <ul>
+          <li>After completing requests, proactively check other key areas, like the Master data fields, to ensure updates are made to applicable key systems so seamless order process occurs that allows for automation of tasks. When this occurs successfully, CSRs can spend more time engaging with the customers.</li>
+          <li>Validate the accuracy of key details like Ship To, Special Requests, confirming the products requested go to the right place and that their Incoterms are set up correctly. </li>
+          <li>Occasionally, for some manual requests you may review an invoice to validate details. </li>
+          <li>When a new customer is being setup, collaborate with the Commercial team and ensure that Service Level Agreements (SLA) are embedded in all systems in alignment with customer segmentation (where applicable).</li>
+        </ul>`
+    }
+  }
+];
